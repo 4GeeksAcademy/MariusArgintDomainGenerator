7e600f605c746e7ab1209af47d6ffc1a649d4576
Marius Argint <mariusargint@gmail.com>
2025-11-21 18:38:23 +0100

    domain generator app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-import "bootstrap";
-import "./style.css";
+const www = ['www'];
+const pronoun = ['the', 'our', 'my', 'your', 'his'];
+const adj = ['great', 'big', 'wild', 'cool', 'epic'];
+const noun = ['jogger', 'racoon', 'coder', 'ninja', 'pirate'];
+const tld = ['.com', '.es', '.net', '.io', '.org']; // domain endings
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// Generate ALL possible combinations (like in the image, with nested for)
+for (let i = 0; i < www.length; i++) {
+  for (let j = 0; j < pronoun.length; j++) {
+    for (let k = 0; k < adj.length; k++) {
+      for (let l = 0; l < noun.length; l++) {
+        for (let m = 0; m < tld.length; m++) {
+          console.log(
+            `${www[i]}.${pronoun[j]}${adj[k]}${noun[l]}${tld[m]}`
+          );
+        }
+      }
+    }
+  }
+}
